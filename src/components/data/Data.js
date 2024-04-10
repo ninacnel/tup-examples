509ef918570c8d7693451a2b9af560ad7057693e
Nerina Coronel <ncoronel@primerosystems.com>
2024-04-09 22:07:45 -0300

    Importing data

---
+++ src/components/data/Data.js
@@ -0,0 +1,6 @@
+export const allProducts = [
+    { code: 1, name: 'Banana', price: 23, isAvailable: true },
+    { code: 2, name: 'Burger', price: 30, isAvailable: false },
+    { code: 3, name: 'Orange', price: 25, isAvailable: true },
+    { code: 4, name: 'Bread', price: 15, isAvailable: false },
+];
--- src/components/parent/Parent.jsx
+++ src/components/parent/Parent.jsx
@@ -2,16 +2,10 @@ import { Col, Container, Row } from "react-bootstrap";
 import Children from "../children/Children";
 import NewProduct from "../newProduct/NewProduct";
 import { useState } from "react";
-
-const allProducts = [
-    { code: 1, name: 'Banana', price: 23, isAvailable: true },
-    { code: 2, name: 'Burger', price: 30, isAvailable: false },
-    { code: 3, name: 'Orange', price: 25, isAvailable: true },
-    { code: 4, name: 'Bread', price: 15, isAvailable: false },
-];
+import { allProducts } from "../data/Data";
 
 const Parent = () => {
-    const [products, setProducts]= useState(allProducts);
+    const [products, setProducts] = useState(allProducts);
 
     const handleAddNewProduct = (newItem) => {
         const newProduct = {
