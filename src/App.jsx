4adb7973c8c5b90ba4e276b3b9c901423857835f
ninacnel <nerinacoronel00@gmail.com>
2024-05-14 23:12:49 -0300

    last comment ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,79 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import NotFound from './components/routes/NotFound';
-import MainLayout from './components/layout/MainLayout';
-import NewProduct from './components/newProduct/NewProduct';
-import Private from './components/routes/Private';
-import Login from './components/login/Login';
-import ProductInfo from './components/productInfo/ProductInfo';
-import Parent from './components/parent/Parent';
-import { useState } from 'react';
-import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NotFound from "./components/routes/NotFound";
+import MainLayout from "./components/layout/MainLayout";
+import NewProduct from "./components/newProduct/NewProduct";
+import Private from "./components/routes/Private";
+import Login from "./components/login/Login";
+import ProductInfo from "./components/productInfo/ProductInfo";
+import Parent from "./components/parent/Parent";
+import { useState } from "react";
+import "./App.css";
+import Comments from "./components/comments/Comments";
 
 function App() {
-    const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-    const handleSignIn = () => {
-        setIsSignedIn(true);
-    };
+  const handleSignIn = () => {
+    setIsSignedIn(true);
+  };
 
-    const handleSignOut = () => {
-        setIsSignedIn(false);
-    };
+  const handleSignOut = () => {
+    setIsSignedIn(false);
+  };
 
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: (
-                <MainLayout>
-                    <Parent />
-                </MainLayout>
-            ),
-        },
-        {
-            path: "/product-form",
-            element: (
-                <Private isSignedIn={isSignedIn}>
-                    <MainLayout>
-                        <NewProduct />
-                    </MainLayout>
-                </Private>
-            ),
-        },
-        {
-            path: "/login",
-            element: (
-                <MainLayout>
-                    <Login
-                        isSignedIn={isSignedIn}
-                        onLogIn={handleSignIn}
-                        onLogOut={handleSignOut}
-                    />
-                </MainLayout>
-            ),
-        },
-        {
-            path: "/product/:id",
-            element: (
-                <MainLayout>
-                    <ProductInfo />
-                </MainLayout>
-            ),
-        },
-        {
-            path: "*",
-            element: <NotFound />,
-        },
-    ]);
-    return <RouterProvider router={router} />;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <MainLayout>
+          <Parent />
+        </MainLayout>
+      ),
+    },
+    {
+      path: "/product-form",
+      element: (
+        <Private isSignedIn={isSignedIn}>
+          <MainLayout>
+            <NewProduct />
+          </MainLayout>
+        </Private>
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <MainLayout>
+          <Login
+            isSignedIn={isSignedIn}
+            onLogIn={handleSignIn}
+            onLogOut={handleSignOut}
+          />
+        </MainLayout>
+      ),
+    },
+    {
+      path: "/product/:id",
+      element: (
+        <MainLayout>
+          <ProductInfo />
+        </MainLayout>
+      ),
+    },
+    {
+      path: "/comments",
+      element: (
+        <MainLayout>
+          <Comments />
+        </MainLayout>
+      ),
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/newProduct/NewProduct.jsx
+++ src/components/newProduct/NewProduct.jsx
@@ -1,75 +1,77 @@
-import { useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap'
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
 // import PropTypes from 'prop-types';
 
 const NewProduct = () => {
-    const [nameEntered, setNameEntered] = useState('');
-    const [priceEntered, setPriceEntered] = useState(0);
-    const [stockEntered, setStockEntered] = useState(false);
+  const [nameEntered, setNameEntered] = useState("");
+  const [priceEntered, setPriceEntered] = useState(0);
+  const [stockEntered, setStockEntered] = useState(false);
 
-    const handleNameEntered = (event) => {
-        setNameEntered(event.target.value);
-        console.log(event.target.value);
-    }
+  const handleNameEntered = (event) => {
+    setNameEntered(event.target.value);
+    console.log(event.target.value);
+  };
 
-    const handlePriceEntered = (event) => {
-        setPriceEntered(event.target.value);
-    }
+  const handlePriceEntered = (event) => {
+    setPriceEntered(event.target.value);
+  };
 
-    const handleStockEntered = (event) => {
-        setStockEntered(event.target.value);
-    }
+  const handleStockEntered = (event) => {
+    setStockEntered(event.target.value);
+  };
 
-    const handleProductSubmit = (event) => {
-        event.preventDefault();
+  const handleProductSubmit = (event) => {
+    event.preventDefault();
 
-        const productData = {
-            name: nameEntered,
-            price: priceEntered,
-            isAvailable: stockEntered,
-        }
+    const productData = {
+      name: nameEntered,
+      price: priceEntered,
+      isAvailable: stockEntered,
+    };
 
-        onHandleAddNewProduct(productData);
+    onHandleAddNewProduct(productData);
 
-        setNameEntered('');
-        setPriceEntered(0);
-        setStockEntered(false);
-    }
+    setNameEntered("");
+    setPriceEntered(0);
+    setStockEntered(false);
+  };
 
-    return (
-        <Container className="bg-dark rounded">
-            <Form onSubmit={handleProductSubmit}>
-                <Form.Group controlId="product-name">
-                    <Form.Label className='text-light'>Product Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder='Name...'
-                        onChange={handleNameEntered}
-                        value={nameEntered}
-                    />
-                </Form.Group>
-                <Form.Group controlId="product-price">
-                    <Form.Label className='text-light'>Price</Form.Label>
-                    <Form.Control
-                        type="number"
-                        onChange={handlePriceEntered}
-                        value={priceEntered}
-                    />
-                </Form.Group>
-                <Form.Group controlId="product-availability">
-                    <Form.Check
-                        type="checkbox"
-                        className='text-light'
-                        label="Available"
-                        onChange={handleStockEntered}
-                        value={stockEntered}
-                    />
-                </Form.Group>
-                <Button type='submit' variant='warning' className="mb-3 mt-2 ps-5 pe-5">Add</Button>
-            </Form>
-        </Container>
-    )
-}
+  return (
+    <Container className="bg-dark rounded">
+      <Form onSubmit={handleProductSubmit}>
+        <Form.Group controlId="product-name">
+          <Form.Label className="text-light">Product Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Name..."
+            onChange={handleNameEntered}
+            value={nameEntered}
+          />
+        </Form.Group>
+        <Form.Group controlId="product-price">
+          <Form.Label className="text-light">Price</Form.Label>
+          <Form.Control
+            type="number"
+            onChange={handlePriceEntered}
+            value={priceEntered}
+          />
+        </Form.Group>
+        <Form.Group controlId="product-availability">
+          <Form.Check
+            type="checkbox"
+            className="text-light"
+            label="Available"
+            onChange={handleStockEntered}
+            value={stockEntered}
+          />
+        </Form.Group>
+        <Button type="submit" variant="warning" className="mb-3 mt-2 ps-5 pe-5">
+          Add
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
 // NewProduct.propTypes = {
 
